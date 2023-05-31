73e106db4a2c37d9770b0de63c8bcaf9e2bc9f21
Bartosz Cylwik <bartosz.cylwik94@gmail.com>
2023-05-31 11:09:33 +0200

    add theme switcher for demo pages

--- src/demo/routes.tsx
+++ src/demo/routes.tsx
@@ -4,19 +4,23 @@ import { Route, Routes as Switch } from "react-router-dom";
 import demoPages from "./pages";
 import { examplesPages } from "./pages";
 import HomePage from "./HomePage";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Routes(): JSX.Element {
   return (
-    <Switch>
-      <Route path="/" element={<HomePage />} />
-      {demoPages.map(({ pages }) =>
-        pages.map(({ name, path, element }) => (
+    <>
+      <Switch>
+        <Route path="/" element={<HomePage />} />
+        {demoPages.map(({ pages }) =>
+          pages.map(({ name, path, element }) => (
+            <Route key={name} path={path} element={element} />
+          ))
+        )}
+        {examplesPages.map(({ name, path, element }) => (
           <Route key={name} path={path} element={element} />
-        ))
-      )}
-      {examplesPages.map(({ name, path, element }) => (
-        <Route key={name} path={path} element={element} />
-      ))}
-    </Switch>
+        ))}
+      </Switch>
+      <ThemeSwitcher />
+    </>
   );
 }
