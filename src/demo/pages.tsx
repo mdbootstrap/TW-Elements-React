0b435429011b5ec04fba0bf32e496d939b468dce
unknown <a.jakubowski@mdbootstrap.com>
2023-06-02 11:18:41 +0200

    Pagination, Avatar, Badges docs and demos

--- src/demo/pages.tsx
+++ src/demo/pages.tsx
@@ -7,6 +7,9 @@ import CollapsePage from "./pages/components/collapse/CollapsePage";
 //examples pages
 import ButtonExamples from "./pages/components/button/exampleList";
 import CollapseExamples from "./pages/components/collapse/exampleList";
+import BadgesPage from "./pages/components/badges/BadgesPage";
+import AvatarPage from "./pages/components/avatar/AvatarPage";
+import PaginationPage from "./pages/navigation/pagination/PaginationPage";
 
 interface Pages {
   name: string;
@@ -22,6 +25,8 @@ interface PagesSection {
 const componentsPages: Pages[] = [
   { name: "button", path: "/components/button", element: <ButtonPage /> },
   { name: "collapse", path: "/components/collapse", element: <CollapsePage /> },
+  { name: "avatar", path: "/components/avatar", element: <AvatarPage /> },
+  { name: "badge", path: "/components/badges", element: <BadgesPage /> },
 ];
 
 const contentStylesPages: Pages[] = [];
@@ -32,7 +37,9 @@ const formsPages: Pages[] = [];
 
 const methodsPages: Pages[] = [];
 
-const navigationPages: Pages[] = [];
+const navigationPages: Pages[] = [
+  { name: "pagination", path: "/navigation/[agination", element: <PaginationPage /> },
+];
 
 const designBlocksPages: Pages[] = [];
 
