9b7405e1519d50992b68fa7b8e014a5df45c65fc
unknown <a.jakubowski@mdbootstrap.com>
2023-06-07 14:31:40 +0200

    fix cards width in demos

--- src/demo/pages/components/cards/examples/CardBasicExample.tsx
+++ src/demo/pages/components/cards/examples/CardBasicExample.tsx
@@ -5,7 +5,7 @@ export default function CardBasicExample(): JSX.Element {
   return (
     <div className="flex justify-center space-x-2">
       <div
-        className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        className="block max-w-[22rem] rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <h5
           className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           Card title
--- src/demo/pages/components/cards/examples/CardWithHeaderAndFooterExample.tsx
+++ src/demo/pages/components/cards/examples/CardWithHeaderAndFooterExample.tsx
@@ -5,7 +5,7 @@ export default function CardWithHeaderAndFooterExample(): JSX.Element {
   return (
     <div className="flex justify-center space-x-2">
       <div
-        className="block rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        className="block max-w-[22rem] rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <div
           className="border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
           Featured
