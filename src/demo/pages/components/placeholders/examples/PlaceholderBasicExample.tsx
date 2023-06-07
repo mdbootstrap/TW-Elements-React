77aff3162f189a092a9924aae9cf06fa86400936
unknown <a.jakubowski@mdbootstrap.com>
2023-06-07 14:50:46 +0200

    fixes in demo, metadata after cr

--- src/demo/pages/components/placeholders/examples/PlaceholderBasicExample.tsx
+++ src/demo/pages/components/placeholders/examples/PlaceholderBasicExample.tsx
@@ -55,7 +55,6 @@ export default function PlaceholderBasicExample(): JSX.Element {
               className="inline-block min-h-[1em] w-8/12 flex-auto cursor-wait bg-current align-middle opacity-50"></span>
           </p>
           <a
-            href="#"
             tabIndex={-1}
             className="inline-block min-h-[1em] w-6/12 flex-auto cursor-wait rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white opacity-50 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out before:inline-block before:content-[''] hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"></a>
         </div>
