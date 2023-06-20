cf158e66d97037a1225d19a26fc0481e0652ac31
Bartosz Cylwik <52455076+juujisai@users.noreply.github.com>
2023-06-20 12:48:07 +0200

    Update docs (#64)
    * Docs: add react page to /docs/react route
    
    * update te react logo
    
    * Add fullscreen option to demo-iframe

--- src/demo/ThemeSwitcher.tsx
+++ src/demo/ThemeSwitcher.tsx
@@ -43,7 +43,9 @@ const svg = {
   ),
 };
 
-const ThemeSwitcher: React.FC = (): JSX.Element => {
+const ThemeSwitcher: React.FC<{ isNotExample: boolean }> = ({
+  isNotExample,
+}): JSX.Element => {
   const switcher = useRef<HTMLDivElement | null>(null);
   const switcherButton = useRef<HTMLButtonElement>(null);
 
@@ -138,106 +140,110 @@ const ThemeSwitcher: React.FC = (): JSX.Element => {
 
   return (
     <>
-      <div
-        className="fixed right-5 bottom-5 z-[9999]"
-        id="theme-switcher"
-        ref={switcher}
-      >
-        <div className="relative" data-te-dropdown-ref>
-          <button
-            ref={switcherButton}
-            className="w-[30px] h-[30px] text-neutral-800 dark:text-white uppercase rounded-full hover:shadow-lg hover:bg-neutral-200 dark:hover:bg-neutral-700 focus:bg-neutral-300 dark:focus:bg-neutral-700 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex items-center justify-center whitespace-nowrap motion-reduce:transition-none"
-            type="button"
-            id="themeSwitcher"
-            data-te-dropdown-toggle-ref
-            aria-expanded="false"
-            onClick={() => setShowDropdown(!showDropdown)}
-          >
-            {svg[activeTheme as keyof typeof svg]}
-          </button>
-          <ul
-            className={dropdownMenuClasses}
-            aria-labelledby="themeSwitcher"
-            data-te-dropdown-menu-ref
-          >
-            <li className="scale-[0.8] text py-1 flex justify-center items-center text-gray-400 font-bold">
-              <svg
-                className="-ml-1 fill-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-                height="20"
-                width="20"
-              >
-                <path d="M7 17v-5.792H3L10 2l7 9.208h-4V17Zm1.5-1.5h3V9.708h2.438L10 4.438l-3.938 5.27H8.5ZM10 9.708Z" />
-              </svg>
-              <span className="ml-1 mr-2">+</span>
-              <span>D</span>
-            </li>
-            <li>
-              <a
-                className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
-                style={
-                  activeTheme === "light" ? { color: "rgb(101,144,213)" } : {}
-                }
-                data-theme="light"
-                data-te-dropdown-item-ref
-                onClick={setLightTheme}
-              >
-                <div className="pointer-events-none">
-                  <div
-                    className="inline-block w-[24px] text-center"
-                    data-theme-icon="light"
-                  >
-                    {svg.light}
+      {isNotExample && (
+        <div
+          className="fixed right-5 bottom-5 z-[9999]"
+          id="theme-switcher"
+          ref={switcher}
+        >
+          <div className="relative" data-te-dropdown-ref>
+            <button
+              ref={switcherButton}
+              className="w-[30px] h-[30px] text-neutral-800 dark:text-white uppercase rounded-full hover:shadow-lg hover:bg-neutral-200 dark:hover:bg-neutral-700 focus:bg-neutral-300 dark:focus:bg-neutral-700 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex items-center justify-center whitespace-nowrap motion-reduce:transition-none"
+              type="button"
+              id="themeSwitcher"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              {svg[activeTheme as keyof typeof svg]}
+            </button>
+            <ul
+              className={dropdownMenuClasses}
+              aria-labelledby="themeSwitcher"
+              data-te-dropdown-menu-ref
+            >
+              <li className="scale-[0.8] text py-1 flex justify-center items-center text-gray-400 font-bold">
+                <svg
+                  className="-ml-1 fill-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="20"
+                  width="20"
+                >
+                  <path d="M7 17v-5.792H3L10 2l7 9.208h-4V17Zm1.5-1.5h3V9.708h2.438L10 4.438l-3.938 5.27H8.5ZM10 9.708Z" />
+                </svg>
+                <span className="ml-1 mr-2">+</span>
+                <span>D</span>
+              </li>
+              <li>
+                <a
+                  className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
+                  style={
+                    activeTheme === "light" ? { color: "rgb(101,144,213)" } : {}
+                  }
+                  data-theme="light"
+                  data-te-dropdown-item-ref
+                  onClick={setLightTheme}
+                >
+                  <div className="pointer-events-none">
+                    <div
+                      className="inline-block w-[24px] text-center"
+                      data-theme-icon="light"
+                    >
+                      {svg.light}
+                    </div>
+                    <span data-theme-name="light">Light</span>
                   </div>
-                  <span data-theme-name="light">Light</span>
-                </div>
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
-                style={
-                  activeTheme === "dark" ? { color: "rgb(101,144,213)" } : {}
-                }
-                data-theme="dark"
-                data-te-dropdown-item-ref
-                onClick={setDarkTheme}
-              >
-                <div className="pointer-events-none">
-                  <div
-                    className="inline-block w-[24px] text-center"
-                    data-theme-icon="dark"
-                  >
-                    {svg.dark}
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
+                  style={
+                    activeTheme === "dark" ? { color: "rgb(101,144,213)" } : {}
+                  }
+                  data-theme="dark"
+                  data-te-dropdown-item-ref
+                  onClick={setDarkTheme}
+                >
+                  <div className="pointer-events-none">
+                    <div
+                      className="inline-block w-[24px] text-center"
+                      data-theme-icon="dark"
+                    >
+                      {svg.dark}
+                    </div>
+                    <span data-theme-name="dark">Dark</span>
                   </div>
-                  <span data-theme-name="dark">Dark</span>
-                </div>
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
-                style={
-                  activeTheme === "system" ? { color: "rgb(101,144,213)" } : {}
-                }
-                data-theme="system"
-                data-te-dropdown-item-ref
-                onClick={setSystemTheme}
-              >
-                <div className="pointer-events-none">
-                  <div
-                    className="inline-block w-[24px] text-center"
-                    data-theme-icon="system"
-                  >
-                    {svg.system}
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm py-2 px-3 font-normal block w-full whitespace-nowrap bg-transparent text-neutral-700 dark:text-neutral-100 hover:bg-neutral-100 disabled:text-neutral-400 disabled:pointer-events-none disabled:bg-transparent active:no-underline active:text-neutral-800 dark:hover:bg-neutral-600 focus:outline-none focus:bg-neutral-200 focus:dark:bg-neutral-600"
+                  style={
+                    activeTheme === "system"
+                      ? { color: "rgb(101,144,213)" }
+                      : {}
+                  }
+                  data-theme="system"
+                  data-te-dropdown-item-ref
+                  onClick={setSystemTheme}
+                >
+                  <div className="pointer-events-none">
+                    <div
+                      className="inline-block w-[24px] text-center"
+                      data-theme-icon="system"
+                    >
+                      {svg.system}
+                    </div>
+                    <span data-theme-name="system">System</span>
                   </div>
-                  <span data-theme-name="system">System</span>
-                </div>
-              </a>
-            </li>
-          </ul>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
