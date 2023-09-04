83a3da09dc0db1bf625a7638ec59c09ab65da088
igor_przybysz <i.przybysz@mdbootstrap.com>
2023-09-04 09:03:40 +0200

    Delete prop disabled in TETextarea demo Basic example

--- src/demo/pages/forms/textarea/examples/TextareaBasicExample.tsx
+++ src/demo/pages/forms/textarea/examples/TextareaBasicExample.tsx
@@ -5,12 +5,7 @@ export default function TextareaBasicExample(): JSX.Element {
   return (
     <div className="flex justify-center">
       <div className="relative mb-3 md:w-96 pt-5">
-        <TETextarea
-          id="textareaExample"
-          label="Message"
-          rows={4}
-          disabled
-        ></TETextarea>
+        <TETextarea id="textareaExample" label="Message" rows={4}></TETextarea>
       </div>
     </div>
   );
