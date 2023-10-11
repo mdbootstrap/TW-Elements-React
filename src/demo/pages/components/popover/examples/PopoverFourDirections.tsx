2eb578def3111a920c18b488a3c7b620b45af739
igor_przybysz <i.przybysz@mdbootstrap.com>
2023-10-11 13:00:51 +0200

    Fix placement in Four directions example

--- src/demo/pages/components/popover/examples/PopoverFourDirections.tsx
+++ src/demo/pages/components/popover/examples/PopoverFourDirections.tsx
@@ -9,20 +9,20 @@ import {
 export default function PopoverFourDirections(): JSX.Element {
   return (
     <div className="grid grid-cols-1 gap-1 text-center sm:gap-0 sm:flex sm:space-x-1 sm:justify-center py-24">
-      <TEPopover placement="top">
+      <TEPopover>
         <TERipple rippleColor="light">
           <TEPopoverToggler className="max-w-[160px] rounded bg-primary-100 px-6 py-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200">
             Popover on top
           </TEPopoverToggler>
         </TERipple>
 
-        <TEPopoverContent>
+        <TEPopoverContent placement="top">
           <div className="p-4 text-[#212529] rounded-lg shadow-[0_0px_3px_0_rgba(0,0,0,0.07),0_2px_2px_0_rgba(0,0,0,0.04)] bg-white bg-clip-padding border border-t-0 border-neutral-100 empty:hidden dark:text-white dark:border-0 dark:bg-neutral-700">
             Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
           </div>
         </TEPopoverContent>
       </TEPopover>
-      <TEPopover placement="right" className="text-left">
+      <TEPopover className="text-left">
         <TERipple rippleColor="light">
           <TEPopoverToggler className="max-w-[160px] rounded bg-primary-100 px-6 py-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200">
             Popover on right
@@ -35,27 +35,27 @@ export default function PopoverFourDirections(): JSX.Element {
           </div>
         </TEPopoverContent>
       </TEPopover>
-      <TEPopover placement="bottom">
+      <TEPopover>
         <TERipple rippleColor="light">
           <TEPopoverToggler className="max-w-[180px] rounded bg-primary-100 px-6 py-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200">
             Popover on bottom
           </TEPopoverToggler>
         </TERipple>
 
-        <TEPopoverContent>
+        <TEPopoverContent placement="bottom">
           <div className="p-4 text-[#212529] rounded-lg shadow-[0_0px_3px_0_rgba(0,0,0,0.07),0_2px_2px_0_rgba(0,0,0,0.04)] bg-white bg-clip-padding border border-t-0 border-neutral-100 empty:hidden dark:text-white dark:border-0 dark:bg-neutral-700">
             Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
           </div>
         </TEPopoverContent>
       </TEPopover>
-      <TEPopover placement="left" className="text-right">
+      <TEPopover className="text-right">
         <TERipple rippleColor="light">
           <TEPopoverToggler className="max-w-[160px] rounded bg-primary-100 px-6 py-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200">
             Popover on left
           </TEPopoverToggler>
         </TERipple>
 
-        <TEPopoverContent>
+        <TEPopoverContent placement="left">
           <div className="p-4 text-[#212529] rounded-lg shadow-[0_0px_3px_0_rgba(0,0,0,0.07),0_2px_2px_0_rgba(0,0,0,0.04)] bg-white bg-clip-padding border border-t-0 border-neutral-100 empty:hidden dark:text-white dark:border-0 dark:bg-neutral-700">
             Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
           </div>
