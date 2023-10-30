47da716a30293a68aa285628a1709103f648303e
Bartosz Cylwik <bartosz.cylwik94@gmail.com>
2023-10-30 12:19:47 +0100

    Add copy dropdown to prism snippets

--- site/static/js/react/new-prism.js
+++ site/static/js/react/new-prism.js
@@ -2762,7 +2762,7 @@ Prism.languages.scss["atrule"].inside.rest = Prism.languages.scss;
     var linkCopy = document.createElement("button");
     linkCopy.innerHTML = "Copy";
     linkCopy.classList =
-      "btn-copy-code text-gray-500 text-xs leading-[1.6] !top-[16px] bg-transparent font-bold uppercase text-sm px-4 py-2 outline-none focus:outline-none";
+      "btn-copy-code text-gray-500 text-xs leading-[1.6] !top-[16px] lg:!right-16 bg-transparent font-bold uppercase text-sm px-4 py-2 outline-none focus:outline-none";
 
     if (!ClipboardJS) {
       callbacks.push(registerClipboard);
@@ -2789,6 +2789,87 @@ Prism.languages.scss["atrule"].inside.rest = Prism.languages.scss;
 
         resetText();
       });
+
+      createSubCopy()
+    }
+
+    function createSubCopy() {
+      const newButton = document.createElement("div");
+      newButton.className = "hidden lg:block absolute text-gray-500 text-xs leading-[1.6] !top-[17px] right-0 bg-transparent font-bold uppercase text-sm px-4 outline-none focus:outline-none"
+      newButton.setAttribute("data-te-dropdown-ref", "")
+
+      newButton.innerHTML = `
+        <button
+          class="px-4 py-2"
+          type="button"
+          id="dropdownMenuButton1"
+          data-te-dropdown-toggle-ref
+          aria-expanded="false"
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </button>
+        <ul
+          class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block normal-case"
+          aria-labelledby="dropdownMenuButton1"
+          data-te-dropdown-menu-ref>
+          <li>
+            <a
+              class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-xs font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600 cursor-pointer"
+              data-te-dropdown-item-ref
+              >Copy template</a
+            >
+          </li>
+          <li>
+            <a
+              class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-xs font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600 cursor-pointer"
+              data-te-dropdown-item-ref
+              >Copy scripts</a
+            >
+          </li>
+          <li>
+            <a
+              class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-xs font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600 cursor-pointer"
+              data-te-dropdown-item-ref
+              >Copy imports</a
+            >
+          </li>
+        </ul>
+      `
+      const items = newButton.querySelectorAll("[data-te-dropdown-item-ref]")
+
+      items.forEach(item => {
+        item.addEventListener("click", (e) => {
+            const content = e.target.textContent;
+            let codeToCopy = env.code;
+    
+            if (content === "Copy template") {
+                codeToCopy = extractCodeBlock(codeToCopy, "return (", ");\n}");
+            } else if (content === "Copy scripts") {
+                const separators = ["React.FC = () => {", "function App() {", "JSX.Element {"];
+                const separator = separators.find(sep => codeToCopy.includes(sep)) || "JSX.Element {";
+                codeToCopy = extractCodeBlock(codeToCopy, separator, "return (\n");
+            } else {
+                codeToCopy = codeToCopy.split("export default function")[0];
+            }
+    
+            codeToCopy = codeToCopy || "";
+            copyToClipboard(codeToCopy);
+        });
+      });
+      
+      function extractCodeBlock(code, start, end) {
+          const startIndex = code.indexOf(start);
+          const endIndex = code.indexOf(end, startIndex + start.length);
+          return (startIndex !== -1 && endIndex !== -1) ? code.substring(startIndex + start.length, endIndex).trim() : "";
+      }
+      
+      function copyToClipboard(text) {
+          navigator.clipboard.writeText(text);
+      }
+      
+      linkCopy?.parentNode?.appendChild(newButton);
     }
 
     function resetText() {
