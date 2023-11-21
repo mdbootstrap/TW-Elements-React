b797abc195ac3c10811341889f5c918d0f310bee
Mateusz Lazaru <m.lazaru@mdbootstrap.com>
2023-11-21 10:04:38 +0100

    stepper - imported stepper in index.tsx, created separated demo examples

---
+++ src/demo/pages/components/stepper/examples/StepperControlledStep.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { TEStepper, TEStepperStep } from "tw-elements-react";
+
+export default function StepperControlledStep(): JSX.Element {
+  const [activeStep, setActiveStep] = useState<number>(1);
+
+  return (
+    <TEStepper
+      activeStep={activeStep}
+      onChange={(stepId) => {
+        setActiveStep(stepId);
+      }}
+    >
+      <TEStepperStep headText="Step 1" headIcon={"1"}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </TEStepperStep>
+      <TEStepperStep headText="Step 2" headIcon={"2"}>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
+      </TEStepperStep>
+      <TEStepperStep headText="Step 3" headIcon={"3"}>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+      </TEStepperStep>
+    </TEStepper>
+  );
+}
